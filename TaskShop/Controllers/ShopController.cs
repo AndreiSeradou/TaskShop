a6e9b04d195360c29bffeaf042f07ee2f02308ba
AndreiSeradou <andreiseradou@gmail.com>
2021-10-13 10:46:10 +0300

    RT

--- TaskShop/Controllers/ShopController.cs
+++ TaskShop/Controllers/ShopController.cs
@@ -20,18 +20,18 @@ namespace TaskShop.Controllers
         }
         private async void addData()
         {
-            await _shop.AddShopAsync(new Shop { Id = 1, Name = "Adidas", Address = "syrganovo 23", OperatingMode = "SHOOSE shop" });
-            await _shop.AddShopAsync(new Shop { Id = 2, Name = "Nike", Address = "chekalovo 4", OperatingMode = "SHOOSE shop" });
-            await _shop.AddShopAsync(new Shop { Id = 3, Name = "Abibas", Address = "fluchko 55", OperatingMode = "SHOOSE shop" });
-            await _product.AddProductAsync(new Product { Id = 1, Name = "Adidas14", Description = "Adidas product", ShopId = 1 });
-            await _product.AddProductAsync(new Product { Id = 2, Name = "PEGAS", Description = "Nike product", ShopId = 2 });
-            await _product.AddProductAsync(new Product { Id = 3, Name = "Abibas V140", Description = "Abibas product", ShopId = 3 });
-            await _product.AddProductAsync(new Product { Id = 1, Name = "Adidas FROK", Description = "Adidas product", ShopId = 1 });
-            await _product.AddProductAsync(new Product { Id = 2, Name = "PEGAS HAMA", Description = "Nike product", ShopId = 2 });
-            await _product.AddProductAsync(new Product { Id = 3, Name = "Abibas TOOL", Description = "Abibas product", ShopId = 3 });
-            await _product.AddProductAsync(new Product { Id = 1, Name = "Adidas", Description = "Adidas product", ShopId = 1 });
-            await _product.AddProductAsync(new Product { Id = 2, Name = "PEGASP", Description = "Nike product", ShopId = 2 });
-            await _product.AddProductAsync(new Product { Id = 3, Name = "Abibas VS", Description = "Abibas product", ShopId = 3 });
+            await _shop.AddShopAsync(new Shop {  Name = "Adidas", Address = "syrganovo 23", OperatingMode = "SHOOSE shop" });
+            await _shop.AddShopAsync(new Shop {  Name = "Nike", Address = "chekalovo 4", OperatingMode = "SHOOSE shop" });
+            await _shop.AddShopAsync(new Shop {  Name = "Abibas", Address = "fluchko 55", OperatingMode = "SHOOSE shop" });
+            await _product.AddProductAsync(new Product {  Name = "Adidas14", Description = "Adidas product", ShopId = 1 });
+            await _product.AddProductAsync(new Product {  Name = "PEGAS", Description = "Nike product", ShopId = 2 });
+            await _product.AddProductAsync(new Product {  Name = "Abibas V140", Description = "Abibas product", ShopId = 3 });
+            await _product.AddProductAsync(new Product {  Name = "Adidas FROK", Description = "Adidas product", ShopId = 1 });
+            await _product.AddProductAsync(new Product {  Name = "PEGAS HAMA", Description = "Nike product", ShopId = 2 });
+            await _product.AddProductAsync(new Product {  Name = "Abibas TOOL", Description = "Abibas product", ShopId = 3 });
+            await _product.AddProductAsync(new Product {  Name = "Adidas", Description = "Adidas product", ShopId = 1 });
+            await _product.AddProductAsync(new Product {  Name = "PEGASP", Description = "Nike product", ShopId = 2 });
+            await _product.AddProductAsync(new Product {  Name = "Abibas VS", Description = "Abibas product", ShopId = 3 });
         }
         public ViewResult ShopList()
         {
